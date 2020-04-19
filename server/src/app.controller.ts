404e9a135e2d70d0129fa4b74f422c6f6c95b43a
Dimitar <dimitar.rskv@gmail.com>
2020-04-19 02:05:00 +0200

    dev: setup base nestjs/angular projects

---
+++ server/src/app.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(
+    private authService: AuthService,
+    private appService: AppService
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ server/src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+    // TODO: export secret to .env variable
+    secret: 'secretKey',
+};
