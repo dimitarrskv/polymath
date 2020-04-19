a59f78dac7762008d8161b3117d8e1c712e7659b
Dimitar <dimitar.rskv@gmail.com>
2020-04-19 03:32:38 +0200

    dev(users): connect users' module to db entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,16 +7,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'polymath',
-      entities: [],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     AuthModule,
     UsersModule
   ],
