da9c3406d4a9d8e5d779b9ba10586387d23da526
Dimitar <dimitar.rskv@gmail.com>
2020-04-23 01:30:22 +0200

    dev: add tailwind css

---
+++ client/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: () => [
+              require("postcss-import"),
+              require("tailwindcss"),
+              require("autoprefixer")
+            ]
+          }
+        }
+      ]
+    },
+  
+    devServer: {
+      disableHostCheck: true
+    }
+};
+  
