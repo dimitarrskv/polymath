bb1e8fa53ffd4e73407dbeb09dfc51ccacb9dcb9
Dimitar <dimitar.rskv@gmail.com>
2020-06-01 00:37:52 +0200

    dev: layout component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,16 +4,21 @@ import { LoginComponent } from './login/login.component';
 import { LoginSuccessComponent } from './login-success/login-success.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { UnauthGuard } from './core/guards/unauth.guard';
+import { LayoutComponent } from './core/components/layout/layout.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-
   { path: 'login/success/:jwt', component: LoginSuccessComponent },
   { path: 'login', component: LoginComponent, canActivate: [UnauthGuard] },
-
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule), 
-  canActivate: [AuthGuard] },
-  { path: 'profile', loadChildren: () => import('./my-profile/my-profile.module').then(m => m.MyProfileModule) }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+      { path: 'profile', loadChildren: () => import('./my-profile/my-profile.module').then(m => m.MyProfileModule) }
+    ],
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../auth.service';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+
+    NgZorroAntdModule
   ],
   providers: [AuthService]
 })
